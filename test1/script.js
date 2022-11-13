3aa066fca73f6b6023fb627323eec9138ac02ce9
LudwigEffort <dr.feynman65@gmail.com>
2022-11-14 00:51:12 +0100

    first commit

---
+++ test1/script.js
@@ -0,0 +1,27 @@
+let x = 2;
+let y = 5;
+
+let sum = x + y;
+
+console.log(sum);
+
+
+const g = 9.81;
+
+let m = parseInt(prompt('Inserisci la massa'));
+
+let F = m * g;
+
+
+console.log(F);
+
+function test(max,min) {
+  if (max > min) {
+    max = 2;
+  }
+  else {
+    max = 3;
+  }
+}
+
+let x = 1;
